2de48c605eca6f944a4807cf5a98c8651f49ac59
Femmy <femson_101@yahoo.co.uk>
2022-05-30 05:41:56 +0000

    added composition part1

--- dele.py
+++ dele.py
@@ -1,6 +1,10 @@
 I am healed in Jesus name...Amen
-I am more than conqueror
+I am more than the conquerors
 I am the head and not the tail
-Above only in Jesus Name
-I am blessed beyond all curses
-Amen and amen!
+Above only in Jesus name
+I am blessed beyond all ancenstral curses
+Amen, amen  and amen!
+the act of combining parts or elements to form a whole.
+the resulting state or product.
+manner of being composed; structure:
+This painting has an orderly composition.
