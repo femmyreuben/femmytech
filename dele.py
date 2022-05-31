02e714ecf718d5742d0e91f348a635dbad423248
Femmy <femson_101@yahoo.co.uk>
2022-05-31 05:16:18 +0000

    edit to dele.py

--- dele.py
+++ dele.py
@@ -6,9 +6,8 @@ I am blessed beyond all ancenstral curses
 Amen, amen  and amen!
 the act of combining parts or elements to form a whole.
 manner of being composed; structure:
-This painting has an orderly composition.
-makeup; constitution:
 His moral composition was impeccable.
 an aggregate material formed from two or more substances:
 a composition of silver and tin.
-a short essay written as a school exercise.
+a short essay written as a school assignment.
+This is the end of the file for now
