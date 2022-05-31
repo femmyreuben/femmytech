c2db703fbb8136bc15ee98b6a5cf32384a346b89
femmyreuben <105896556+femmyreuben@users.noreply.github.com>
2022-05-30 23:26:23 -0600

    Update dele.py

--- dele.py
+++ dele.py
@@ -4,10 +4,7 @@ I am the head and not the tail
 Above only in Jesus name
 I am blessed beyond all ancenstral curses
 Amen, amen  and amen!
-the act of combining parts or elements to form a whole.
-manner of being composed; structure:
-His moral composition was impeccable.
 an aggregate material formed from two or more substances:
 a composition of silver and tin.
 a short essay written as a school assignment.
-This is the end of the file for now
+This is the end of the file for the new commit
