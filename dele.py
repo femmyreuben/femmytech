5c4758f28db19748ad66237ed632b5b1d0deeb48
femmyreuben <105896556+femmyreuben@users.noreply.github.com>
2022-05-31 19:59:52 -0600

    2nd update

--- dele.py
+++ dele.py
@@ -5,6 +5,6 @@ Above only in Jesus name
 I am blessed beyond all ancenstral curses
 Amen, amen  and amen!
 an aggregate material formed from two or more substances:
-a composition of silver and tin.
-a short essay written as a school assignment.
 This is the end of the file for the new commit
+Testing Git Fetch
+Starship Group
